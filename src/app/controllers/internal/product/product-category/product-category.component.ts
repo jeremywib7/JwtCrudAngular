8c90f1f03cad79283fab9e7c702b49f47f4d9887
jeremywib <jeremywib7@gmail.com>
2022-02-14 23:44:20 +0700

    adding crud categories

--- src/app/controllers/internal/product/product-category/product-category.component.ts
+++ src/app/controllers/internal/product/product-category/product-category.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RxFormBuilder} from "@rxweb/reactive-form-validators";
+import {ToastrService} from "ngx-toastr";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductService} from "../../../../_services/product.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {EMPTY} from "rxjs";
 
 @Component({
   selector: 'app-product-category',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private rxFormBuilder: RxFormBuilder,
+    private toastr: ToastrService,
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private ngbModal: NgbModal,
+    private router: Router,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.loadAllCategories().then(r => EMPTY);
+  }
+
+
+  async loadAllCategories() {
+    await this.productService.loadAllProductCategory().subscribe({
+      next: value => {
+        console.log(value);
+      },
+      error: error => {
+        this.router.navigate(['/int/product']);
+      },
+      complete: () => {
+      },
+    });
   }
 
 }
